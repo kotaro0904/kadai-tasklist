9f01e2b9bea8ce90289940696ad740f5e300b8fc
kotaro.ito <kotaro0904yakyu@gmail.com>
2019-12-03 10:36:55 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   get 'toppages/index'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-    root to: 'tasks#index'
+  #root to: 'tasks#index'
+  root to: "toppages#index"
     
     get 'signup', to: 'users#new'
     resources :users, only: [:index, :show, :new, :create]
+    resources :tasks
 end
