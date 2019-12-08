2edcf9468366f1558d499bdce86bdce61a0e3955
kotaro.ito <kotaro0904yakyu@gmail.com>
2019-12-08 11:12:14 +0000

    user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,11 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  
   def index
-      @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 
   def show
@@ -13,7 +17,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = current_user.tasks.build(task_params)
+    @task = current_user.tasks.build(tasks_params)
     if @task.save
       flash[:success] = 'Taskを投稿しました。'
       redirect_to root_url
@@ -55,7 +59,14 @@ class TasksController < ApplicationController
   end
     
  # Strong Parameter
-  def task_params
+  def tasks_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
